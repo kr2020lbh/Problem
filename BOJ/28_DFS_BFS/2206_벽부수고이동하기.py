05f8875f3355b354a2fb1f0604e56c8a184e53cd
kr2020lbh <eric99kr@gmail.com>
2020-09-13 12:55:11 +0900

    file upload

--- BOJ/28_DFS_BFS/2206_벽부수고이동하기.py
+++ BOJ/28_DFS_BFS/2206_벽부수고이동하기.py
@@ -3,12 +3,15 @@ sys.stdin = open("input.txt","r")
 di = [1,0,-1,0]
 dj = [0,1,0,-1]
 def dfs(i,j,cnt,wall_break):
-
+    [print(a) for a in arr]
+    print()
     if i == N-1 and j == M-1:
         return
     for k in range(4):
         d_i = i+di[k]
         d_j = j+dj[k]
+        if [d_i,d_j] in visited:
+            continue
         if 0<= d_i < N and 0<= d_j < M:
             if (arr[d_i][d_j] != 0) and (arr[d_i][d_j] !=-1):
                 continue
@@ -18,11 +21,10 @@ def dfs(i,j,cnt,wall_break):
                     if wall_break==True:
                         continue
 
-                arr[d_i][d_j] = cnt
+                arr[d_i][d_j] = cnt+1
+                visited.append([d_i,d_j])
                 dfs(d_i, d_j, cnt + 1, True)
-
-def bfs():
-
+                visited.pop()
 
 
 N,M = map(int,input().split())
@@ -32,6 +34,8 @@ for i in range(N):
         if arr[i][j] == 1:
             arr[i][j] = -1
 arr[0][0] = 1
+visited = []
 dfs(0,0,1,False)
+
 print(arr[N-1][M-1]-1)
 
