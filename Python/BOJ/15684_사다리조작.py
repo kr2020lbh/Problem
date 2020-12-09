e271df9bc6dfcbfb1371f69857d41eeff7519055
kr2020lbh <eric99kr@gmail.com>
2020-12-09 23:54:59 +0900

    file upload

---
+++ Python/BOJ/15684_사다리조작.py
@@ -0,0 +1,73 @@
+import sys
+from itertools import combinations
+sys.stdin = open("input.txt","r")
+
+
+def sol(ladder_cnt):
+    for loc in combinations(indexes,ladder_cnt):
+        # print('before', loc)
+        # [print(l) for l in ladders]
+        put_ladder(loc,1)
+        # print('after')
+        # [print(l) for l in ladders]
+        if go_ladder():
+            return True
+
+        put_ladder(loc,0)
+    return False
+
+def put_ladder(loc,loc_val):
+    for x, y in loc:
+        ladders[x][y] = loc_val
+        ladders[x][y + 1] = -loc_val
+
+def go_ladder():
+
+    index = 0
+    while index < N-1:
+        col = index
+        row = 0
+        # print()
+        # [print(l) for l in ladders]
+
+        while row <= H:
+            # print(index, row, col)
+
+            if row == H:
+                if index != col:
+                    return False
+                break
+
+            if ladders[row][col] == 1:
+                    col += 1
+            elif ladders[row][col] == -1:
+                    col -= 1
+            row += 1
+        index += 1
+    return True
+
+
+N,M,H = map(int,input().split())
+ladders = [[0]*N for _ in range(H)]
+indexes = []
+for _ in range(M):
+    x,y = map(int,input().split())
+    ladders[x-1][y-1] = 1
+    ladders[x-1][y] = -1
+# [print(l) for l in ladders]
+for x in range(H):
+    for y in range(N):
+        if y+1 < N and ladders[x][y]==0 and ladders[x][y+1] == 0 :
+            indexes.append([x,y])
+
+
+#0행 c 열에서 시작한다
+#각각 c열에서 밑으로 떨어지면서 1이 있으면 다음 열로 이동한다
+#그러다가 마지막 행이 되었을 떄, 자기 자신의 열을 반환하여 모두 자기 열이 되면 옳은 것!
+#3개까지 놓고 그 이후는 -1로..
+for ladder_cnt in range(4):
+    if sol(ladder_cnt):
+        print(ladder_cnt)
+        break
+else:
+    print(-1)
